655f7f89174355b68ec98b7f10f9069fe6974b12
Ben Moore <ben.moore83@outlook.com.au>
2022-10-08 16:43:51 +1000

    Deploy prep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+//import { defineConfig } from 'astro/config'; export default defineConfig({});
 
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig } from 'astro/config'
+export default defineConfig({site: 'https://twitchingastronaut.github.io', base: '/resume.new',})
